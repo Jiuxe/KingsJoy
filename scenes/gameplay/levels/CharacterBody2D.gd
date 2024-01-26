d658a57bb2ac2cff151ce852d449aeb021db7088
Brian Holsters <brian.holsters@turningtables.global>
2024-01-27 00:17:51 +0100

    brian cleanup

--- scenes/gameplay/levels/CharacterBody2D.gd
+++ scenes/gameplay/levels/CharacterBody2D.gd
@@ -1,12 +1,10 @@
 extends CharacterBody2D
+@onready var collision_shape_2d = $CollisionShape2D
 
-
-const SPEED = 300.0
 const JUMP_VELOCITY = -800.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -16,7 +14,7 @@ func _physics_process(delta):
 	move_and_slide()
 
 func _draw():
-	draw_circle(Vector2.ZERO, $CollisionShape2D.shape.radius, "fff")
+	draw_circle(Vector2.ZERO, collision_shape_2d.shape.radius, "fff")
 
 func jump():
 	self.velocity.y = JUMP_VELOCITY
