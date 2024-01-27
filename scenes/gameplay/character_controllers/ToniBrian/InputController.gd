d44fa1b0cadee75203b5b4873ac77efee844bd18
Antonio Godoy Gonzalez <43647734+AntonioGodoyUGR@users.noreply.github.com>
2024-01-27 15:55:21 +0100

    change character controller

--- scenes/gameplay/character_controllers/ToniBrian/InputController.gd
+++ scenes/gameplay/character_controllers/ToniBrian/InputController.gd
@@ -16,11 +16,12 @@ func _physics_process(delta):
 		return
 	
 	balance_movement(delta)
+	normal_movement(delta)
 	
-	if Input.is_action_just_pressed("ui_left"):
+	if Input.is_action_just_pressed("left_arm"):
 		actor.l_arm_collider.activate()
 	
-	if Input.is_action_just_pressed("ui_right"):
+	if Input.is_action_just_pressed("right_arm"):
 		actor.r_arm_collider.activate()
 	
 	if Input.is_action_just_pressed("ui_accept"):
@@ -29,37 +30,23 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("ui_cancel"):
 		Game.restart_scene()
 
-func control_arm(arm, action, inverted:=false):
-	var inversion = -1 if inverted else 1
-	if Input.is_action_pressed(action):
-		arm.rotation_degrees = lerp(arm.rotation_degrees, 140.0 * inversion, 0.8)
-	else:
-		arm.rotation_degrees = lerp(arm.rotation_degrees, 0.0, 0.8)
-
 func normal_movement(delta):
 	var h_input := Input.get_axis("ui_left", "ui_right")
 	actor.angle += h_input * rotation_speed * delta
 
 func balance_movement(delta):
-	if Input.is_action_pressed("ui_left"):
+	if Input.is_action_pressed("left_arm"):
 		l_arm.rotation_degrees = lerp(l_arm.rotation_degrees, 140.0, 0.9)
 		bending_l = true
 	else:
 		l_arm.rotation_degrees = lerp(l_arm.rotation_degrees, 0.0, 0.9)
 		bending_l = false
 	
-	if Input.is_action_pressed("ui_right"):
+	if Input.is_action_pressed("right_arm"):
 		r_arm.rotation_degrees = lerp(r_arm.rotation_degrees, -140.0, 0.8)
 		bending_r = true
 	else:
 		r_arm.rotation_degrees = lerp(r_arm.rotation_degrees, 0.0, 0.8)
 		bending_r = false
-	
-	control_arm(r_arm, "ui_right", true)
 
-	var h_input = 0
-	if bending_l:
-		h_input += 1
-	if bending_r:
-		h_input -= 1
-	actor.angle += h_input * rotation_speed * delta
+	
