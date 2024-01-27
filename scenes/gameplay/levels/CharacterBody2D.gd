4590d372ace0800f864a0c49d0cd7bae9bccabc4
Brian Holsters <brian.holsters@turningtables.global>
2024-01-27 12:11:19 +0100

    yoquese

--- scenes/gameplay/levels/CharacterBody2D.gd
+++ scenes/gameplay/levels/CharacterBody2D.gd
@@ -1,4 +1,5 @@
 extends CharacterBody2D
+
 @onready var collision_shape_2d = $CollisionShape2D
 
 const JUMP_VELOCITY = -800.0
@@ -10,7 +11,6 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
 	move_and_slide()
 
 func _draw():
